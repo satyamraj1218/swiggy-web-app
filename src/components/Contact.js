f1bbcd86123098b835b19c971e6b25a7b6fd0e8c
Satyam Raj <satyamraj1218@gmail.com>
2025-01-04 18:45:30 +0530

    react-router

---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact me at:</h1>
+      <h3>Email: satyamraj1218@gmail.com</h3>
+      <h3>Mobile: 7355198507</h3>
+    </div>
+  );
+};
+
+export default Contact;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,3 +5,6 @@ export const CDN_URL = 'https://media-assets.swiggy.com/swiggy/image/upload/';
 
 export const SWIGGY_WEB_API =
   'https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
+
+export const MENU_API =
+  'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=';
